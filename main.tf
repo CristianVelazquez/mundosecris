409e0a86048346b7cdc56c141050c8422fa0ba78
Daniel-MundosE <mundose@ojcius.com>
2021-09-22 19:41:48 -0300

    Terraform Script - Crear Instancia

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  version = "~> 2.7"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "web" {  
+  ami = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+}
+
+output "public_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
